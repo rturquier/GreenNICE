b6408599f09ab9252b13144f7d87d125a0e36cb4
iroliva <iroliva@ucdavis.edu>
2024-10-24 09:30:35 -0700

    Create function to plot welfare as a function of params

--- src/runs_params.jl
+++ src/runs_params.jl
@@ -19,15 +19,17 @@ t[!, Symbol("welfare, θ = 0.5")] = tt
 t
 
 vector = 0.1:0.1:1.0
-params = []
 
-function plot_welfare_world(model, values_α, values_θ, values_η)
+function plot_welfare_world(model, values, param)
 
     # Run the model and store values
     first_iteration = true
     results = DataFrame()
 
-    for x in values_param
+
+if param == 1
+
+    for x in values
         set_param!(model, :θ, x)
         run(model)
 
@@ -51,9 +53,66 @@ function plot_welfare_world(model, values_α, values_θ, values_η)
         title="Global Welfare Over Time for Different θ Values"
     )
     return fig
+end #end case θ
+
+if param == 2 
+    for x in values
+        set_param!(model, :α, x)
+        run(model)
+
+        if first_iteration
+            results = getdataframe(model, :welfare=>:welfare_global)
+            rename!(results, :welfare_global => Symbol("α = $x"))
+            first_iteration = false
+        else
+            welfare_data = model[:welfare, :welfare_global]
+            results[!, Symbol("α = $x")] = welfare_data
+        end
+    end
+
+    results_long = stack(results, Not(:time), variable_name=:α, value_name=:welfare_global)
+
+    fig = results_long |> @vlplot(
+        :line, 
+        x=:time, 
+        y=:welfare_global, 
+        color=:α,
+        title="Global Welfare Over Time for Different α Values"
+    )
+    return fig
+
+end #end case α
+
+if param == 3
+    for x in values_θ
+        set_param!(model, :η, x)
+        run(model)
+
+        if first_iteration
+            results = getdataframe(model, :welfare=>:welfare_global)
+            rename!(results, :welfare_global => Symbol("η = $x"))
+            first_iteration = false
+        else
+            welfare_data = model[:welfare, :welfare_global]
+            results[!, Symbol("η = $x")] = welfare_data
+        end
+    end
+
+    results_long = stack(results, Not(:time), variable_name=:η, value_name=:welfare_global)
+
+    fig = results_long |> @vlplot(
+        :line, 
+        x=:time, 
+        y=:welfare_global, 
+        color=:η,
+        title="Global Welfare Over Time for Different η Values"
+    )
+    return fig
+end #end case η
+
 end
 
-A = plot_welfare_world(plot_1, vector)
+A = plot_welfare_world(plot_1, vector, 2)
 
 
 

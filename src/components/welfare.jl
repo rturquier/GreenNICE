e94e3e2a380ff21bb648bc166ccf22f59152d550
rturquier <57546969+rturquier@users.noreply.github.com>
2024-11-06 16:10:12 -0800

    🥇  Declare @defcomp first
    Having the component declaration at the
    top of the file makes it easier to understand.

--- src/components/welfare.jl
+++ src/components/welfare.jl
@@ -1,3 +1,77 @@
+@defcomp welfare begin
+
+    country         = Index()
+    regionwpp       = Index()
+	quantile        = Index()
+
+    qcpc_post_recycle       = Parameter(index=[time, country, quantile])    # Quantile per capita consumption after recycling tax back to quantiles (thousand USD2017 per person per year)
+    η                       = Parameter()                                   # Inequality aversion
+    nb_quantile             = Parameter()                                   # Number of quantiles
+    l                       = Parameter(index=[time, country])              # Population (thousands)
+    mapcrwpp                = Parameter(index=[country])                    # Map from country index to wpp region index
+
+    cons_EDE_country        = Variable(index=[time, country])               # Equally distributed welfare equivalent consumption (thousand USD2017 per person per year)
+    cons_EDE_rwpp           = Variable(index=[time, regionwpp])             # Regional qually distributed welfare equivalent consumption (thousand USD2017 per person per year)
+    cons_EDE_global         = Variable(index=[time])                        # Glibal equally distributed welfare equivalent consumption (thousand USD2017 per person per year)
+    welfare_country         = Variable(index=[time, country])               # Country welfare
+    welfare_rwpp            = Variable(index=[time, regionwpp])             # WPP region welfare
+    welfare_global          = Variable(index=[time])                        # Global welfare
+
+    α                       = Parameter()                                   # Environmental good weight in utility function
+    θ                       = Parameter()                                   # Elasticity of substitution between consumption and environmental good
+    Env                     = Parameter(index=[time, country, quantile])    # Environmental good consumption (**Unit to be defined**). Does not vary by quantile
+    GreenNice               = Parameter()                                   # GreenNice switch (1 = ON)
+    E_bar                   = Parameter()                                   # Reference level of environment
+    Env_country             = Variable(index=[time, country])
+    Env_rwpp                = Variable(index=[time, regionwpp])
+
+    function run_timestep(p, v, d, t)
+
+        for c in d.country
+            v.cons_EDE_country[t,c] = EDE(
+                p.qcpc_post_recycle[t,c,:],
+                p.Env[t,c,:],
+                p.E_bar,
+                p.η,
+                p.θ,
+                p.α,
+                p.nb_quantile
+            )
+
+            v.welfare_country[t,c] = (
+                (p.l[t,c] / p.nb_quantile) * sum(utility.(
+                    p.qcpc_post_recycle[t,c,:], p.Env[t,c,:], p.η, p.θ, p.α
+                ))
+            )
+        end # country loop
+
+        for rwpp in d.regionwpp
+            country_indices = findall(x -> x == rwpp, p.mapcrwpp)
+
+            v.cons_EDE_rwpp[t,rwpp] = EDE_aggregated(
+                v.cons_EDE_country[t,country_indices],
+                p.E_bar,
+                p.η,
+                p.θ,
+                p.α,
+                p.l[t,country_indices]
+            )
+            v.welfare_rwpp[t,rwpp] = sum(v.welfare_country[t,country_indices])
+        end # region loop
+
+        v.cons_EDE_global[t] = EDE_aggregated(
+            v.cons_EDE_country[t,:],
+            p.E_bar,
+            p.η,
+            p.θ,
+            p.α,
+            p.l[t,:],
+        )
+        v.welfare_global[t] = sum(v.welfare_country[t,:])
+    end # timestep
+end
+
+
 function utility(consumption, environment, η, θ, α)
     if η == 1
         utility = log(
@@ -43,80 +117,4 @@ function EDE_aggregated(country_level_EDE, baseline_environment, η, θ, α, pop
     average_utility = total_utility / total_population
     aggregated_EDE = inverse_utility(average_utility, baseline_environment, η, θ, α)
     return aggregated_EDE
-end
-
-
-@defcomp welfare begin
-
-    country         = Index()
-    regionwpp       = Index()
-	quantile        = Index()
-
-    qcpc_post_recycle       = Parameter(index=[time, country, quantile])    # Quantile per capita consumption after recycling tax back to quantiles (thousand USD2017 per person per year)
-    η                       = Parameter()                                   # Inequality aversion
-    nb_quantile             = Parameter()                                   # Number of quantiles
-    l                       = Parameter(index=[time, country])              # Population (thousands)
-    mapcrwpp                = Parameter(index=[country])                    # Map from country index to wpp region index
-
-    cons_EDE_country        = Variable(index=[time, country])               # Equally distributed welfare equivalent consumption (thousand USD2017 per person per year)
-    cons_EDE_rwpp           = Variable(index=[time, regionwpp])             # Regional qually distributed welfare equivalent consumption (thousand USD2017 per person per year)
-    cons_EDE_global         = Variable(index=[time])                        # Glibal equally distributed welfare equivalent consumption (thousand USD2017 per person per year)
-    welfare_country         = Variable(index=[time, country])               # Country welfare
-    welfare_rwpp            = Variable(index=[time, regionwpp])             # WPP region welfare
-    welfare_global          = Variable(index=[time])                        # Global welfare
-
-
-    α                       = Parameter()                                   # Environmental good weight in utility function
-    θ                       = Parameter()                                   # Elasticity of substitution between consumption and environmental good
-    Env                     = Parameter(index=[time, country, quantile])    # Environmental good consumption (**Unit to be defined**). Does not vary by quantile
-    GreenNice               = Parameter()                                   # GreenNice switch (1 = ON)
-    E_bar                   = Parameter()                                   # Reference level of environment
-    Env_country             = Variable(index=[time, country])
-    Env_rwpp                = Variable(index=[time, regionwpp])
-
-    function run_timestep(p, v, d, t)
-
-        for c in d.country
-
-            v.cons_EDE_country[t,c] = EDE(
-                p.qcpc_post_recycle[t,c,:],
-                p.Env[t,c,:],
-                p.E_bar,
-                p.η,
-                p.θ,
-                p.α,
-                p.nb_quantile
-            )
-
-            v.welfare_country[t,c] = (
-                (p.l[t,c] / p.nb_quantile) * sum(utility.(
-                    p.qcpc_post_recycle[t,c,:], p.Env[t,c,:], p.η, p.θ, p.α
-                ))
-            )
-        end # country loop
-
-        for rwpp in d.regionwpp
-            country_indices = findall(x -> x == rwpp, p.mapcrwpp)
-
-            v.cons_EDE_rwpp[t,rwpp] = EDE_aggregated(
-                v.cons_EDE_country[t,country_indices],
-                p.E_bar,
-                p.η,
-                p.θ,
-                p.α,
-                p.l[t,country_indices]
-            )
-            v.welfare_rwpp[t,rwpp] = sum(v.welfare_country[t,country_indices])
-        end # region loop
-
-        v.cons_EDE_global[t] = EDE_aggregated(
-            v.cons_EDE_country[t,:],
-            p.E_bar,
-            p.η,
-            p.θ,
-            p.α,
-            p.l[t,:],
-        )
-        v.welfare_global[t] = sum(v.welfare_country[t,:])
-    end # timestep
 end

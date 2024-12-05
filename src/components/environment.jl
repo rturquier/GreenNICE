292c144f71b158da0644d228b8f274b4b41d1fc9
iroliva <iroliva@ucdavis.edu>
2024-12-05 14:48:33 -0800

    Get time series for Env at regional and global levels

--- src/components/environment.jl
+++ src/components/environment.jl
@@ -6,11 +6,14 @@
     nb_quantile     = Parameter()                           # Number of quantiles
     damage          = Parameter()                           # percetage loss of E over time
     Env0            = Parameter(index=[country])            # Initial level of environmental good
+    mapcrwpp        = Parameter(index=[country])
 
     Env             = Variable(index=[time, country, quantile])     # Environmental variable
     Env_percapita   = Variable(index=[time, country, quantile])     #E percapita
     E_bar           = Variable()                                    # Average level of environment per capita at time 0
     Env_country     = Variable(index=[time,country])
+    Env_rwpp        = Variable(index=[time, regionwpp])
+    Env_global      = Variable(index=[time])
 
     function run_timestep(p, v, d, t)
         # Note that the country dimension is defined in d and parameters and variables are indexed by 'c'
@@ -26,10 +29,19 @@
                 (1 / p.nb_quantile) * (sum(v.Env[t, c, :]) / p.l[t, c])
         end
 
+        #Evolution of Env over time for plots
+
         for c in d.country, q in d.quantile
             v.Env_country[t,c] = sum(v.Env[t,c,:])
         end
 
-        return v.E_bar = sum(p.Env0[:]) / sum(p.l[TimestepIndex(1), :])
+        for rwpp in d.regionwpp
+            country_indices = findall(x -> x == rwpp, p.mapcrwpp)
+            v.Env_rwpp[t,rwpp] = sum(v.Env_country[t,country_indices])
+        end
+
+        v.Env_global[t] = sum(v.Env_country[t,:])
+
+        v.E_bar = sum(p.Env0[:]) / sum(p.l[TimestepIndex(1), :])
     end
 end #end component

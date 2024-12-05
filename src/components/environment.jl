42c3ef58c4c05d2b2c4fec5ff7995d1cb08b4dad
iroliva <iroliva@ucdavis.edu>
2024-12-04 16:10:48 -0800

    Calculate e0 per capita and use it in welfare

--- src/components/environment.jl
+++ src/components/environment.jl
@@ -3,21 +3,28 @@
     country                 = Index()                           #Note that a regional index is defined here
     quantile		        = Index()
 
+    l                       = Parameter(index=[time, country]) # Labor - population (thousands)
+    nb_quantile             = Parameter()                      # Number of quantiles
+
+
     Env0                    = Parameter(index=[country])   # Initial level of environmental good
     Env                     = Variable(index=[time, country, quantile])   # Environmental variable
+    Env_percapita           = Variable(index=[time, country, quantile])     #E percapita
     E_bar                   = Variable()                        # Average level of environment at time 0
 
     function run_timestep(p, v, d, t)
     # Note that the country dimension is defined in d and parameters and variables are indexed by 'c'
 
-        v.E_bar = mean(p.Env0)/1000
-
         for c in d.country, q in d.quantile
-            v.Env[t,c,q] = 100
-            #is_first(t) ? p.Env0[c]/100 : v.Env[t-1,c,q]
+            v.Env[t,c,q] = is_first(t) ? (p.Env0[c] / p.nb_quantile) : v.Env[t-1,c,q]
         end
 
-    end
+        for c in d.country, q in d.quantile
+            #Assume environmental good is equally distributed
+            v.Env_percapita[t,c,q] = (1 / p.nb_quantile) * (sum(v.Env[t,c,:]) / p.l[t,c])
+        end
 
+        v.E_bar = sum(p.Env0[:]) / sum(p.l[TimestepIndex(1),:])
+    end
 
 end #end component
--- src/components/welfare.jl
+++ src/components/welfare.jl
@@ -19,7 +19,7 @@
 
     α                       = Parameter()                                   # Environmental good weight in utility function
     θ                       = Parameter()                                   # Elasticity of substitution between consumption and environmental good
-    Env                     = Parameter(index=[time, country, quantile])    # Environmental good consumption (**Unit to be defined**). Does not vary by quantile
+    Env_percapita           = Parameter(index=[time, country, quantile])    # Non_market environmental good
     E_bar                   = Parameter()                                   # Reference level of environment
     Env_country             = Variable(index=[time, country])
     Env_rwpp                = Variable(index=[time, regionwpp])
@@ -29,7 +29,7 @@
         for c in d.country
             v.cons_EDE_country[t,c] = EDE(
                 p.qcpc_post_recycle[t,c,:],
-                p.Env[t,c,:],
+                p.Env_percapita[t,c,:],
                 p.E_bar,
                 p.η,
                 p.θ,
@@ -39,7 +39,7 @@
 
             v.welfare_country[t,c] = (
                 (p.l[t,c] / p.nb_quantile) * sum(utility.(
-                    p.qcpc_post_recycle[t,c,:], p.Env[t,c,:], p.η, p.θ, p.α
+                    p.qcpc_post_recycle[t,c,:], p.Env_percapita[t,c,:], p.η, p.θ, p.α
                 ))
             )
         end # country loop

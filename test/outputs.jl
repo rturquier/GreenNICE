f5a21b6dbc69cd6d43aba5209ff178c3a284520f
iroliva <iroliva@ucdavis.edu>
2024-12-11 13:28:53 -0800

    Make table for global decile analysis

---
+++ test/outputs.jl
@@ -0,0 +1,59 @@
+### Results Analysis
+
+using Pkg
+Pkg.activate(joinpath(@__DIR__, ".."))
+#Pkg.resolve() # To resolve inconsistencies between Manifest.toml and Project.toml
+Pkg.instantiate()
+using Mimi, MimiFAIRv2, DataFrames, CSVFiles
+
+include("../src/GreenNICE.jl")
+
+base_model = GreenNICE.create()
+
+run(base_model)
+
+EDE_decile = EDE_globaldecile(base_model)
+
+function EDE_globaldecile(
+model
+)
+EDE_decilecountry = model[:welfare, :cons_EDE_decilecountry]
+t0 = 1
+l = base_model[:welfare, :l]
+nb_quantile = model[:welfare, :nb_quantile]
+
+total_population_0 = sum(l[t0, :])
+
+df = DataFrame(Country = Int64[], Decile = Int64[], EDE_init = Float64[], Pop = Float64[])
+
+    for c in 1:size(EDE_decilecountry, 2)
+        for d in 1:size(EDE_decilecountry, 3)
+            push!(df, (c, d, EDE_decilecountry[t0, c, d], (l[t0,c] / nb_quantile) ))
+        end
+    end
+
+sort!(df, 3)
+
+df.key = string.(df.Country, "_", df.Decile)
+
+df.cumulative_population = cumsum(df[:, 4])
+
+df.global_decile = 10 .* ceil.(df.cumulative_population ./ total_population_0, digits = 1)
+
+EDE_globaldecile = DataFrame(year = Int64[], Country = Int64[], Decile = Int64[], EDE = Float64[])
+
+for t in 1:size(EDE_decilecountry,1)
+    for c in 1:size(EDE_decilecountry, 2)
+        for d in 1:size(EDE_decilecountry, 3)
+            push!(EDE_globaldecile, (t, c, d, EDE_decilecountry[t, c, d]))
+        end
+    end
+end
+
+EDE_globaldecile.key = string.(EDE_globaldecile.Country, "_", EDE_globaldecile.Decile)
+
+EDE_globaldecile = leftjoin(EDE_globaldecile, df[:, ["key", "global_decile"]], on = :key)
+
+return EDE_globaldecile
+
+end

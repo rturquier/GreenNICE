d1a949fa8d5a421aa495915d9579ddd140dbbd1d
iroliva <iroliva@ucdavis.edu>
2024-12-12 13:24:54 -0800

    Add environment functions

--- src/components/environment.jl
+++ src/components/environment.jl
@@ -14,6 +14,7 @@
     Env_country     = Variable(index=[time,country])
     Env_rwpp        = Variable(index=[time, regionwpp])
     Env_global      = Variable(index=[time])
+    Env_decile      = Variable(index=[time, quantile])
 
     function run_timestep(p, v, d, t)
         # Note that the country dimension is defined in d and parameters and variables are indexed by 'c'
@@ -42,6 +43,21 @@
 
         v.Env_global[t] = sum(v.Env_country[t,:])
 
+        for q in d.quantile
+            v.Env_decile[t,q] = Env_distribution(v.Env[t,:,q], p.l[t,:], p.nb_quantile)
+        end
+
         v.E_bar = sum(p.Env0[:]) / sum(p.l[TimestepIndex(1), :])
     end
 end #end component
+
+function Env_distribution(
+    environment,
+    population,
+    nb_quantile
+)
+    total_population = sum(population)
+    Env_decile = (1 / total_population) * sum( (population ./ nb_quantile) .*
+    environment)
+    return Env_decile
+end

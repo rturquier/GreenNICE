bd483c93ee98d0a03c43f5d897f16493793e07b2
iroliva <iroliva@ucdavis.edu>
2025-03-06 09:29:57 -0800

    improve code

--- test/assess_env.jl
+++ test/assess_env.jl
@@ -12,11 +12,9 @@ Pkg.instantiate()
 # Load required Julia packages.
 using Mimi, MimiFAIRv2, DataFrames, CSVFiles, Plots
 
-println("Load NICE2020 source code.")
-# Load NICE2020 source code.
 include("../src/GreenNICE.jl")
-
 include("functions_analysis.jl")
+
 m = GreenNICE.create()
 
 ## Set E and parameters
@@ -24,17 +22,17 @@ update_param!(m, :environment, :dam_assessment, 1)
 
 run(m)
 
-Damages_2030 = get_env_damages_year(m, 2200)
+Damages_2200 = get_env_damages_year(m, 2200)
 
-AAA = plot_env_damages!(Damages_2030,
-                        "Percent change in non-market natural capital by 2200",
-                        "Percentage_loss_env")
+plot_env_damages!(Damages_2200,
+                    "Percent change in non-market natural capital by 2200",
+                    "Percentage_loss_env")
 
 Damages_1c = get_env_damage_temp(m, 1)
 
-BBB = plot_env_damages!(Damages_1c,
-                        "Percentage changes in non-market natural capital with a 1C increase",
-                        "Percentage_loss_1c")
+plot_env_damages!(Damages_1c,
+                    "Percentage changes in non-market natural capital with a 1C increase",
+                    "Percentage_loss_1c")
 
 
 

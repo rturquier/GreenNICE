182006221e4d95aa32e757ef9f2399016b6843f2
iroliva <iroliva@ucdavis.edu>
2025-09-09 16:10:55 -0700

    Incorporate changes in income distribution Include \gamma parameter

--- src/components/quantile_recycle.jl
+++ src/components/quantile_recycle.jl
@@ -22,6 +22,7 @@
     damage_elasticity        	= Parameter()                          			# Income elasticity of climate damages (1 = proportional to income)
 	quantile_consumption_shares = Parameter(index=[time, country, quantile]) 	# Income shares of deciles
     recycle_share           	= Parameter(index=[country, quantile]) 		    # Share of carbon tax revenue recycled back to each quantile
+    γ                           = Parameter()                                   # Change in income distribution parameter
 
     CO2_income_elasticity    	= Variable(index=[time, country])             	# Elasticity of CO2 price exposure with respect to income
     abatement_cost_dist		 	= Variable(index=[time, country, quantile]) 	# Quantile distribution shares of mitigation costs
@@ -44,10 +45,11 @@
 	mu_cons				        = Variable(index=[time, country])				# Parameter mu of the lognormal distribution of consumption
 	sigma_cons			        = Variable(index=[time, country])				# Parameter sigma of the lognormal distribution of consumption
 
+    new_consumption_shares      = Variable(index=[time, country, quantile])     # New consumption shares after change in inequality
 
     function run_timestep(p, v, d, t)
 
-        # temporary variable for global population in t 
+        # temporary variable for global population in t
         temp_pop_global = sum(p.l[t,:])
 
         #temporary variable for quantile population in t (country x quantile)
@@ -74,17 +76,21 @@
             ## Compute consumption distributions for quantiles
             #################################################
 
+           #Update consumption shares for a given γ
+
+           v.new_consumption_shares[t,c,:] = change_inequality(p.quantile_consumption_shares[t,c,:], p.γ)
+
             # Calculate quantile distribution shares of CO2 tax burden and mitigation costs (assume both distributions are equal) and climate damages.
-			v.abatement_cost_dist[t,c,:] = country_quantile_distribution(v.CO2_income_elasticity[t,c], p.quantile_consumption_shares[t,c,:], p.nb_quantile)
-			v.carbon_tax_dist[t,c,:]     = country_quantile_distribution(v.CO2_income_elasticity[t,c], p.quantile_consumption_shares[t,c,:], p.nb_quantile)
-			v.damage_dist[t,c,:]         = country_quantile_distribution(p.damage_elasticity, p.quantile_consumption_shares[t,c,:], p.nb_quantile)
+			v.abatement_cost_dist[t,c,:] = country_quantile_distribution(v.CO2_income_elasticity[t,c], v.new_consumption_shares[t,c,:], p.nb_quantile)
+			v.carbon_tax_dist[t,c,:]     = country_quantile_distribution(v.CO2_income_elasticity[t,c], v.new_consumption_shares[t,c,:], p.nb_quantile)
+			v.damage_dist[t,c,:]         = country_quantile_distribution(p.damage_elasticity, v.new_consumption_shares[t,c,:], p.nb_quantile)
 
 			# Create a temporary variable used to calculate NICE baseline quantile consumption (just for convenience).
 			temp_qcpc = p.nb_quantile * p.CPC[t,c] * (1.0 + p.LOCAL_DAMFRAC_KW[t,c]) / (1.0 - p.ABATEFRAC[t,c])
 			for q in d.quantile
 
 				# Calculate pre-damage, pre-abatement cost quantile consumption.
-				v.qcpc_base[t,c,q] = temp_qcpc * p.quantile_consumption_shares[t,c,q]
+				v.qcpc_base[t,c,q] = temp_qcpc * v.new_consumption_shares[t,c,q]
 
 				# Calculate post-damage, post-abatement cost per capita quantile consumption (bounded below to ensure consumptions don't collapse to zero or go negative).
 				# Note, this differs from standard NICE equation because quantile CO2 abatement cost and climate damage shares can now vary over time.
@@ -115,7 +121,7 @@
             v.qc_share[t,c,:] .= v.qcpc_post_recycle[t,c,:]./ v.sum_qcpc_post_recycle[t,c]*100
 
             #################
-            ## Compute Gini 
+            ## Compute Gini
             ################
 
 			#gini consumption
@@ -138,4 +144,25 @@
         v.gini_cons_global[t] = gini(convert(Vector{Real},vec(v.qcpc_post_recycle[t,:,:])), convert(Vector{Real},vec(temp_pop_quantile))) *100
 
     end # timestep
+end
+
+"""
+change_inequality(consumption_shares::matrix, γ::Real)
+
+Change income distribution by a given γ
+
+# Arguments
+- quantile_consumption_shares::Matrix: matrix with consumption shares (3-dimensional)
+- γ::Real: parameter that controls the change in inequality.
+"""
+
+function change_inequality(quantile_consumption_shares::Vector, γ::Real)
+
+    average_consumption = mean(quantile_consumption_shares)
+
+    δ = (quantile_consumption_shares .- average_consumption) ./ average_consumption
+
+    new_quantile_consumption_shares = average_consumption .* (1 .+ δ .* γ)
+
+    return new_quantile_consumption_shares
 end

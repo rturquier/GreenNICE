b79dab0bf5abd4166f2b49a4fa80891e60b95937
rturquier <57546969+rturquier@users.noreply.github.com>
2025-09-30 16:39:34 +0200

    🏳️  Edit whitespace for consistency

--- src/components/emissions.jl
+++ src/components/emissions.jl
@@ -6,21 +6,19 @@
     country         = Index()       # Index for countries
     regionwpp       = Index()       # Index for WPP regions
 
-    σ          = Parameter(index=[time, country])   # Emissions output ratio (GtCO2 per million USD2017)
-    YGROSS     = Parameter(index=[time, country])   # Gross output (million USD2017 per year)
-    μ          = Parameter(index=[time, country])   # Emissions control rate
-    mapcrwpp  = Parameter(index=[country])          # Map from country index to WPP region index
-    co2_pulse  = Parameter(index=[time])            # Exogenous pulse(s) of CO2, in tons
+    σ          = Parameter(index=[time, country])  # Emissions output ratio (GtCO2 per million USD2017)
+    YGROSS     = Parameter(index=[time, country])  # Gross output (million USD2017 per year)
+    μ          = Parameter(index=[time, country])  # Emissions control rate
+    mapcrwpp   = Parameter(index=[country])        # Map from country index to WPP region index
+    co2_pulse  = Parameter(index=[time])           # Exogenous pulse(s) of CO2, in tons
 
-    E_gtco2            = Variable(index=[time, country])   # Country level CO₂ emissions (GtCO2 per year)
-    E_Global_gtco2     = Variable(index=[time])            # Global CO₂ emissions (sum of all country emissions) (GtCO2 per year)
-    E_Global_gtc       = Variable(index=[time])            # Global C emissions (GtC per year)
-    E_gtco2_rwpp      = Variable(index=[time, regionwpp])   # Regional CO₂ emissions of WPP regions (GtCO2 per year)
+    E_gtco2        = Variable(index=[time, country])    # Country level CO₂ emissions (GtCO2 per year)
+    E_Global_gtco2 = Variable(index=[time])             # Global CO₂ emissions (sum of all country emissions) (GtCO2 per year)
+    E_Global_gtc   = Variable(index=[time])             # Global C emissions (GtC per year)
+    E_gtco2_rwpp   = Variable(index=[time, regionwpp])  # Regional CO₂ emissions of WPP regions (GtCO2 per year)
 
     function run_timestep(p, v, d, t)
-        # Define an equation for E
         for c in d.country
-
             v.E_gtco2[t,c] = p.YGROSS[t,c] * p.σ[t,c] * (1-p.μ[t,c])
         end
 
@@ -34,7 +32,5 @@
             country_indices = findall(x->x==rwpp , p.mapcrwpp) #Country indices for the region
             v.E_gtco2_rwpp[t,rwpp]= sum(v.E_gtco2[t, country_indices])
         end
-
     end
-
 end

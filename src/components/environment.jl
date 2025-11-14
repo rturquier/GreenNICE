181c8ee0e0e73915017e51987027623df8d6068e
Ignacio Oliva <ign.oliva@gmail.com>
2025-11-14 11:13:49 -0800

    Recode E_discount_rate as constant (#58)
    instead of a parameter

--- src/components/environment.jl
+++ src/components/environment.jl
@@ -22,13 +22,11 @@
     E_flow_rwpp         = Variable(index=[time, regionwpp])             # Flow of natural capital per WPP region (2017 million usd per year)
     E_flow_global       = Variable(index=[time])                        # Flow of natural capital globally (2017 million usd per year)
 
-    E_discount_rate         = Parameter()                               # Discount rate to calculate stock of E (from CWON)
-
-
-
     function run_timestep(p, v, d, t)
 
-        stock_to_flow_factor    = (1 - p.E_discount_rate) / (1 - p.E_discount_rate^100)
+        E_discount_rate = 0.04
+
+        stock_to_flow_factor    = (1 - E_discount_rate) / (1 - E_discount_rate^100)
 
         E_stock0_percapita = (sum(p.E_stock0[:])) / sum(p.l[TimestepIndex(1), :])
 

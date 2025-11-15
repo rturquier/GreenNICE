f05df27b1a34b5a60f93fabe9d513b2478264a16
iroliva <iroliva@ucdavis.edu>
2025-11-14 16:14:32 -0800

    Get first maps * Map interaction costs *Interaction percentage * Use dictionary

--- analysis/scc.jl
+++ analysis/scc.jl
@@ -1,7 +1,7 @@
 using Mimi
 using TidierData
 using TidierFiles
-using VegaLite
+using VegaLite, VegaDatasets
 
 include("../src/GreenNICE.jl")
 using .GreenNICE
@@ -342,3 +342,351 @@ function get_SCC_decomposition_country(
 
     return SCC_decomposition_df
 end
+
+function map_SCC_decomposition_country( SCC_decomposition_df::DataFrame)
+
+    #Calculate interaction
+    no_inequality_df = @chain SCC_decomposition_df begin
+        @filter(γ == 0)
+        @select(country, no_inequality_damage_E = present_cost_of_damages_to_E)
+    end
+
+    inequality_df = @chain SCC_decomposition_df begin
+        @filter(γ == 1)
+        @select(country, inequality_damage_E = present_cost_of_damages_to_E)
+    end
+
+    interaction_df = @chain innerjoin(inequality_df, no_inequality_df, on = :country) begin
+        @mutate(interaction = inequality_damage_E - no_inequality_damage_E)
+        @mutate(interaction_pct = (- interaction) / inequality_damage_E * 100)
+    end
+
+    interaction_df.id = [alpha3_to_numeric[string(iso3)] for iso3 in interaction_df.country]
+
+    #plot
+    world110m = dataset("world-110m")
+
+    map_interaction = @vlplot(
+        width = 640,
+        height = 360,
+        title = "Interaction effect of within country inequality (USD)",
+        projection = {type = :equirectangular}
+    ) +
+    @vlplot(
+        data = {
+            values = world110m,
+            format = {
+                type = :topojson,
+                feature = :countries
+            }
+        },
+        transform = [{
+            lookup = "id",
+            from = {
+                data = interaction_df,
+                key = :id,
+                fields = ["interaction"]
+            }
+        }],
+        mark = :geoshape,
+        encoding = {
+            color = {
+                field = "interaction",
+                type = "quantitative",
+                title = "Interaction cost",
+                scale = {
+                    scheme = "redblue"
+                }
+            }
+        }
+    )
+
+    map_percentage_interaction = @vlplot(
+        width = 640,
+        height = 360,
+        title = "Interaction effect of within country inequality (USD)",
+        projection = {type = :equirectangular}
+    ) +
+    @vlplot(
+        data = {
+            values = world110m,
+            format = {
+                type = :topojson,
+                feature = :countries
+            }
+        },
+        transform = [{
+            lookup = "id",
+            from = {
+                data = interaction_df,
+                key = :id,
+                fields = ["interaction_pct"]
+            }
+        }],
+        mark = :geoshape,
+        encoding = {
+            color = {
+                field = "interaction_pct",
+                type = "quantitative",
+                title = "Interaction percentage change",
+                scale = {
+                    scheme = "redblue"
+                }
+            }
+        }
+    )
+
+end
+
+
+alpha3_to_numeric = Dict(
+    "AFG" => 4,    # Afghanistan
+    "ALB" => 8,    # Albania
+    "DZA" => 12,   # Algeria
+    "ASM" => 16,   # American Samoa
+    "AND" => 20,   # Andorra
+    "AGO" => 24,   # Angola
+    "AIA" => 660,  # Anguilla
+    "ATA" => 10,   # Antarctica
+    "ATG" => 28,   # Antigua and Barbuda
+    "ARG" => 32,   # Argentina
+    "ARM" => 51,   # Armenia
+    "ABW" => 533,  # Aruba
+    "AUS" => 36,   # Australia
+    "AUT" => 40,   # Austria
+    "AZE" => 31,   # Azerbaijan
+    "BHS" => 44,   # Bahamas (the)
+    "BHR" => 48,   # Bahrain
+    "BGD" => 50,   # Bangladesh
+    "BRB" => 52,   # Barbados
+    "BLR" => 112,  # Belarus
+    "BEL" => 56,   # Belgium
+    "BLZ" => 84,   # Belize
+    "BEN" => 204,  # Benin
+    "BMU" => 60,   # Bermuda
+    "BTN" => 64,   # Bhutan
+    "BOL" => 68,   # Bolivia (Plurinational State of)
+    "BES" => 535,  # Bonaire, Sint Eustatius and Saba
+    "BIH" => 70,   # Bosnia and Herzegovina
+    "BWA" => 72,   # Botswana
+    "BVT" => 74,   # Bouvet Island
+    "BRA" => 76,   # Brazil
+    "IOT" => 86,   # British Indian Ocean Territory (the)
+    "BRN" => 96,   # Brunei Darussalam
+    "BGR" => 100,  # Bulgaria
+    "BFA" => 854,  # Burkina Faso
+    "BDI" => 108,  # Burundi
+    "CPV" => 132,  # Cabo Verde
+    "KHM" => 116,  # Cambodia
+    "CMR" => 120,  # Cameroon
+    "CAN" => 124,  # Canada
+    "CYM" => 136,  # Cayman Islands (the)
+    "CAF" => 140,  # Central African Republic (the)
+    "TCD" => 148,  # Chad
+    "CHL" => 152,  # Chile
+    "CHN" => 156,  # China
+    "CXR" => 162,  # Christmas Island
+    "CCK" => 166,  # Cocos (Keeling) Islands (the)
+    "COL" => 170,  # Colombia
+    "COM" => 174,  # Comoros (the)
+    "COD" => 180,  # Congo (the Democratic Republic of the)
+    "COG" => 178,  # Congo (the)
+    "COK" => 184,  # Cook Islands (the)
+    "CRI" => 188,  # Costa Rica
+    "HRV" => 191,  # Croatia
+    "CUB" => 192,  # Cuba
+    "CUW" => 531,  # Curaçao
+    "CYP" => 196,  # Cyprus
+    "CZE" => 203,  # Czechia
+    "CIV" => 384,  # Côte d'Ivoire
+    "DNK" => 208,  # Denmark
+    "DJI" => 262,  # Djibouti
+    "DMA" => 212,  # Dominica
+    "DOM" => 214,  # Dominican Republic (the)
+    "ECU" => 218,  # Ecuador
+    "EGY" => 818,  # Egypt
+    "SLV" => 222,  # El Salvador
+    "GNQ" => 226,  # Equatorial Guinea
+    "ERI" => 232,  # Eritrea
+    "EST" => 233,  # Estonia
+    "SWZ" => 748,  # Eswatini
+    "ETH" => 231,  # Ethiopia
+    "FLK" => 238,  # Falkland Islands (the) [Malvinas]
+    "FRO" => 234,  # Faroe Islands (the)
+    "FJI" => 242,  # Fiji
+    "FIN" => 246,  # Finland
+    "FRA" => 250,  # France
+    "GUF" => 254,  # French Guiana
+    "PYF" => 258,  # French Polynesia
+    "ATF" => 260,  # French Southern Territories (the)
+    "GAB" => 266,  # Gabon
+    "GMB" => 270,  # Gambia (the)
+    "GEO" => 268,  # Georgia
+    "DEU" => 276,  # Germany
+    "GHA" => 288,  # Ghana
+    "GIB" => 292,  # Gibraltar
+    "GRC" => 300,  # Greece
+    "GRL" => 304,  # Greenland
+    "GRD" => 308,  # Grenada
+    "GLP" => 312,  # Guadeloupe
+    "GUM" => 316,  # Guam
+    "GTM" => 320,  # Guatemala
+    "GGY" => 831,  # Guernsey
+    "GIN" => 324,  # Guinea
+    "GNB" => 624,  # Guinea-Bissau
+    "GUY" => 328,  # Guyana
+    "HTI" => 332,  # Haiti
+    "HMD" => 334,  # Heard Island and McDonald Islands
+    "VAT" => 336,  # Holy See (the)
+    "HND" => 340,  # Honduras
+    "HKG" => 344,  # Hong Kong
+    "HUN" => 348,  # Hungary
+    "ISL" => 352,  # Iceland
+    "IND" => 356,  # India
+    "IDN" => 360,  # Indonesia
+    "IRN" => 364,  # Iran (Islamic Republic of)
+    "IRQ" => 368,  # Iraq
+    "IRL" => 372,  # Ireland
+    "IMN" => 833,  # Isle of Man
+    "ISR" => 376,  # Israel
+    "ITA" => 380,  # Italy
+    "JAM" => 388,  # Jamaica
+    "JPN" => 392,  # Japan
+    "JEY" => 832,  # Jersey
+    "JOR" => 400,  # Jordan
+    "KAZ" => 398,  # Kazakhstan
+    "KEN" => 404,  # Kenya
+    "KIR" => 296,  # Kiribati
+    "PRK" => 408,  # Korea (the Democratic People's Republic of)
+    "KOR" => 410,  # Korea (the Republic of)
+    "KWT" => 414,  # Kuwait
+    "KGZ" => 417,  # Kyrgyzstan
+    "LAO" => 418,  # Lao People's Democratic Republic (the)
+    "LVA" => 428,  # Latvia
+    "LBN" => 422,  # Lebanon
+    "LSO" => 426,  # Lesotho
+    "LBR" => 430,  # Liberia
+    "LBY" => 434,  # Libya
+    "LIE" => 438,  # Liechtenstein
+    "LTU" => 440,  # Lithuania
+    "LUX" => 442,  # Luxembourg
+    "MAC" => 446,  # Macao
+    "MDG" => 450,  # Madagascar
+    "MWI" => 454,  # Malawi
+    "MYS" => 458,  # Malaysia
+    "MDV" => 462,  # Maldives
+    "MLI" => 466,  # Mali
+    "MLT" => 470,  # Malta
+    "MHL" => 584,  # Marshall Islands (the)
+    "MTQ" => 474,  # Martinique
+    "MRT" => 478,  # Mauritania
+    "MUS" => 480,  # Mauritius
+    "MYT" => 175,  # Mayotte
+    "MEX" => 484,  # Mexico
+    "FSM" => 583,  # Micronesia (Federated States of)
+    "MDA" => 498,  # Moldova (the Republic of)
+    "MCO" => 492,  # Monaco
+    "MNG" => 496,  # Mongolia
+    "MNE" => 499,  # Montenegro
+    "MSR" => 500,  # Montserrat
+    "MAR" => 504,  # Morocco
+    "MOZ" => 508,  # Mozambique
+    "MMR" => 104,  # Myanmar
+    "NAM" => 516,  # Namibia
+    "NRU" => 520,  # Nauru
+    "NPL" => 524,  # Nepal
+    "NLD" => 528,  # Netherlands (the)
+    "NCL" => 540,  # New Caledonia
+    "NZL" => 554,  # New Zealand
+    "NIC" => 558,  # Nicaragua
+    "NER" => 562,  # Niger (the)
+    "NGA" => 566,  # Nigeria
+    "NIU" => 570,  # Niue
+    "NFK" => 574,  # Norfolk Island
+    "MNP" => 580,  # Northern Mariana Islands (the)
+    "NOR" => 578,  # Norway
+    "OMN" => 512,  # Oman
+    "PAK" => 586,  # Pakistan
+    "PLW" => 585,  # Palau
+    "PSE" => 275,  # Palestine, State of
+    "PAN" => 591,  # Panama
+    "PNG" => 598,  # Papua New Guinea
+    "PRY" => 600,  # Paraguay
+    "PER" => 604,  # Peru
+    "PHL" => 608,  # Philippines (the)
+    "PCN" => 612,  # Pitcairn
+    "POL" => 616,  # Poland
+    "PRT" => 620,  # Portugal
+    "PRI" => 630,  # Puerto Rico
+    "QAT" => 634,  # Qatar
+    "MKD" => 807,  # Republic of North Macedonia
+    "ROU" => 642,  # Romania
+    "RUS" => 643,  # Russian Federation (the)
+    "RWA" => 646,  # Rwanda
+    "REU" => 638,  # Réunion
+    "BLM" => 652,  # Saint Barthélemy
+    "SHN" => 654,  # Saint Helena, Ascension and Tristan da Cunha
+    "KNA" => 659,  # Saint Kitts and Nevis
+    "LCA" => 662,  # Saint Lucia
+    "MAF" => 663,  # Saint Martin (French part)
+    "SPM" => 666,  # Saint Pierre and Miquelon
+    "VCT" => 670,  # Saint Vincent and the Grenadines
+    "WSM" => 882,  # Samoa
+    "SMR" => 674,  # San Marino
+    "STP" => 678,  # Sao Tome and Principe
+    "SAU" => 682,  # Saudi Arabia
+    "SEN" => 686,  # Senegal
+    "SRB" => 688,  # Serbia
+    "SYC" => 690,  # Seychelles
+    "SLE" => 694,  # Sierra Leone
+    "SGP" => 702,  # Singapore
+    "SXM" => 534,  # Sint Maarten (Dutch part)
+    "SVK" => 703,  # Slovakia
+    "SVN" => 705,  # Slovenia
+    "SLB" => 90,   # Solomon Islands
+    "SOM" => 706,  # Somalia
+    "ZAF" => 710,  # South Africa
+    "SGS" => 239,  # South Georgia and the South Sandwich Islands
+    "SSD" => 728,  # South Sudan
+    "ESP" => 724,  # Spain
+    "LKA" => 144,  # Sri Lanka
+    "SDN" => 729,  # Sudan (the)
+    "SUR" => 740,  # Suriname
+    "SJM" => 744,  # Svalbard and Jan Mayen
+    "SWE" => 752,  # Sweden
+    "CHE" => 756,  # Switzerland
+    "SYR" => 760,  # Syrian Arab Republic
+    "TWN" => 158,  # Taiwan (Province of China)
+    "TJK" => 762,  # Tajikistan
+    "TZA" => 834,  # Tanzania, United Republic of
+    "THA" => 764,  # Thailand
+    "TLS" => 626,  # Timor-Leste
+    "TGO" => 768,  # Togo
+    "TKL" => 772,  # Tokelau
+    "TON" => 776,  # Tonga
+    "TTO" => 780,  # Trinidad and Tobago
+    "TUN" => 788,  # Tunisia
+    "TUR" => 792,  # Turkey
+    "TKM" => 795,  # Turkmenistan
+    "TCA" => 796,  # Turks and Caicos Islands (the)
+    "TUV" => 798,  # Tuvalu
+    "UGA" => 800,  # Uganda
+    "UKR" => 804,  # Ukraine
+    "ARE" => 784,  # United Arab Emirates (the)
+    "GBR" => 826,  # United Kingdom of Great Britain and Northern Ireland (the)
+    "UMI" => 581,  # United States Minor Outlying Islands (the)
+    "USA" => 840,  # United States of America (the)
+    "URY" => 858,  # Uruguay
+    "UZB" => 860,  # Uzbekistan
+    "VUT" => 548,  # Vanuatu
+    "VEN" => 862,  # Venezuela (Bolivarian Republic of)
+    "VNM" => 704,  # Viet Nam
+    "VGB" => 92,   # Virgin Islands (British)
+    "VIR" => 850,  # Virgin Islands (U.S.)
+    "WLF" => 876,  # Wallis and Futuna
+    "ESH" => 732,  # Western Sahara
+    "YEM" => 887,  # Yemen
+    "ZMB" => 894,  # Zambia
+    "ZWE" => 716,  # Zimbabwe
+    "ALA" => 248   # Åland Islands
+)

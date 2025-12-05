1165e81552662828b58cf533d295b10e5a514a17
Ignacio Oliva <ign.oliva@gmail.com>
2025-12-05 07:17:56 -0800

    🗺️  Map effect of inequalities by country (#67)
    * SCC_decomposition per country
    make new function
    
    * get_SCC_decomposition_country
    
    * get_SCC_decomposition_country $\gamma$_list
    
    * Get first maps
    * Map interaction costs
    *Interaction percentage
    * Use dictionary
    
    * Add Countries package
    To get numeric country codes
    
    * get_SCC_interaction_country
    calculates SCC interaction  by country. Returns absolute itneraction and inequality percentage contribution.
    
    * improve get_SCC_interaction_country
    
    * map_SCC_decomposition_country
    Plot map of SCC decomposition by country in absolute terms and as percentage of damages to E.
    
    * improve apply_SCC_decomposition_country
    to match original function
    
    * 🎛️  Keep parameters in SCC_decomposition_df (#64)
    
    This should help with issue #61.
    
    Co-authored-by: rturquier <57546969+rturquier@users.noreply.github.com>
    
    * Update apply_SCC_decomposition_formula()
    Works with country-level SCC decomposition
    
    * Update get_SCC_decomposition ()
    Incorporates country_level analysis option
    
    * Update get_SCC_decomposition()
    To work with country_level boolean
    
    * Improve function's descriptions
    To describe country_level argument
    
    * update get_SCC_interaction_country()
    
    * Update map_SCC_decomposition_country
    Remove title and change color pattern
    
    * Fix get_SCC_decomposition
    Removes unnecessary if
    
    * Update functions to work with analysis_level as a kwarg
    apply_SCC_decomposition_formula()
    get_SCC_decomposition
    
    * Update get_SCC_interaction
    to have regional-level analysis
    
    * get_SCC_interaction
    Fix column names for region option
    
    * Generate file for replication
    
    * get_SCC_interaction()
    Fix typos with names
    
    * delete region option
    In get_SCC_interation()
    
    * Update names of map objects
    So they are nouns instead of verbs
    
    * Update variable name to analysis_level
    On several functions.
    
    * Remove unnecessary packages
    
    * Fix comment
    pulse_size is in ton CO2
    
    * Make if clearer
    in apply_SCC_decomposition_formula()
    
    * Fix docstrings
    To match new parameters' names and describe get_SCC_interaction()
    
    * Report economic benefit as negative damages
    get_SCC_interaction()
    
    * Update color option
    To match new interaction_pct in map_SCC_decomposition_pct()
    
    * ◻️  Improve docstrings
    
    * 🐞  Update replication.jl and fix code
    
    * 🏷️  Improve names
    
    ---------
    
    Co-authored-by: rturquier <57546969+rturquier@users.noreply.github.com>

--- analysis/scc.jl
+++ analysis/scc.jl
@@ -1,7 +1,8 @@
 using Mimi
 using TidierData
 using TidierFiles
-using VegaLite
+using VegaLite, VegaDatasets
+using Countries
 
 include("../src/GreenNICE.jl")
 using .GreenNICE
@@ -140,7 +141,8 @@ end
 
 @doc raw"""
         apply_SCC_decomposition_formula(
-            prepared_df::DataFrame, reference_marginal_utility::Real, ρ::Real
+            prepared_df::DataFrame, reference_marginal_utility::Real, ρ::Real;
+            analysis_level::String="global"
         )::DataFrame
 
     Get present value of equity-weighted, money-metric damages to `c` and `E`.
@@ -155,14 +157,26 @@ end
     Marginal damages to consumption ``\frac{dc_i}{de}`` are called `marginal_damage_to_c` in
     the dataframe, and marginal damages to the environment, ``\frac{dE_i}{de}``, are coded
     as `marginal_damage_to_E`.
+
+    analysis_level == "global" computes the SCC decomposition at the global level, while
+    analysis_level == "country" computes the SCC decomposition at the country level.
 """
 function apply_SCC_decomposition_formula(
-    prepared_df::DataFrame, reference_marginal_utility::Real, ρ::Real
+    prepared_df::DataFrame, reference_marginal_utility::Real, ρ::Real;
+    analysis_level::String="global"
 )::DataFrame
+
     β = 1 / (1 + ρ)
+
+    if analysis_level == "country"
+        group_columns = [:country, :year]
+    else
+        group_columns = [:year]
+    end
+
     SCC_df = @eval @chain $prepared_df begin
-        @group_by(year)
-        # Exclude 4 small countries with E = 0 because the have infinite ∂_cE
+        @group_by($(group_columns...))
+        # Exclude 4 small countries with E = 0 because they have infinite ∂_cE
         @filter(E > 0)
         @summarize(
             t = unique(t),
@@ -180,11 +194,10 @@ function apply_SCC_decomposition_formula(
     return SCC_df
 end
 
-
 """
         get_SCC_decomposition(
             η::Real, θ::Real, α::Real, γ::Real, ρ::Real;
-            pulse_year::Int=2025, pulse_size::Real=1.
+            analysis_level::String="global", pulse_year::Int=2025, pulse_size::Real=1.
         )::DataFrame
 
     Get social cost of carbon as damages to consumption and damages to the environment.
@@ -208,12 +221,15 @@ end
     - `γ::Real`: within-country inequality parameter. 0 means no within-country inequality.
         1 is the standard calibration.
     - `ρ::Real`: rate of pure time preference (utility discount rate).
+    - `analysis_level::String`: if "global", computes SCC decomposition at the global level,
+        if "country", computes SCC decomposition at the country level.
     - `pulse_year::Int`: year where the CO2 marginal pulse is emmitted, and year of
         reference for the SCC.
     - `pulse_size::Real`: size of the CO2 pulse, in tons.
 """
 function get_SCC_decomposition(
-    η::Real, θ::Real, α::Real, γ::Real, ρ::Real; pulse_year::Int=2025, pulse_size::Real=1.
+    η::Real, θ::Real, α::Real, γ::Real, ρ::Real;
+    analysis_level::String="global", pulse_year::Int=2025, pulse_size::Real=1.
 )::DataFrame
     mm = set_up_marginal_model(η, θ, α, γ, pulse_year, pulse_size)
     run(mm)
@@ -223,7 +239,10 @@ function get_SCC_decomposition(
 
     SCC_decomposition_df = @eval @chain begin
         prepare_df_for_SCC($model_df, $η, $θ, $α)
-        apply_SCC_decomposition_formula(_, $reference_marginal_utility, $ρ)
+        apply_SCC_decomposition_formula(
+            _, $reference_marginal_utility, $ρ;
+            analysis_level = $analysis_level
+        )
         @mutate(
             η = $η,
             θ = $θ,
@@ -235,16 +254,17 @@ function get_SCC_decomposition(
     return SCC_decomposition_df
 end
 
-
 """
         get_SCC_decomposition(
-        η::Real, θ::Real, α::Real, γ_list::Vector, ρ::Real; kwargs...
-    )::DataFrame
+            η::Real, θ::Real, α::Real, γ_list::Vector, ρ::Real; kwargs...
+        )::DataFrame
 
     Get SCC decomposition for a vector of γ values.
 
     Apply `get_SCC_decomposition` for each value of γ provided in `γ_list`.
-    Return a Dataframe with as many rows as values in `γ_list`, with three columns:
+    Return a Dataframe with as many rows as values in `γ_list`, with five columns:
+    - `η`, equal to the input `η`,
+    - `θ`, equal to the input `θ`,
     - `γ`, equal to `γ_list`,
     - `present_cost_of_damages_to_c`,
     - `present_cost_of_damages_to_E`.
@@ -253,10 +273,9 @@ function get_SCC_decomposition(
     η::Real, θ::Real, α::Real, γ_list::Vector, ρ::Real; kwargs...
 )::DataFrame
     df_list = map(γ -> get_SCC_decomposition(η, θ, α, γ, ρ; kwargs...), γ_list)
-    concatenated_df = reduce(vcat, df_list)
-    return concatenated_df
+    SCC_decomposition_df = reduce(vcat, df_list)
+    return SCC_decomposition_df
 end
-
 
 function plot_SCC_decomposition(SCC_decomposition_df::DataFrame)::VegaLite.VLSpec
     damages_to_E_with_equal_deciles = @chain SCC_decomposition_df begin
@@ -281,4 +300,144 @@ function plot_SCC_decomposition(SCC_decomposition_df::DataFrame)::VegaLite.VLSpe
 
     plot = plot_df |> @vlplot(:area, x="γ:q", y="SCC_part:q", color="interaction:N")
     return plot
+end
+
+"""
+        function get_SCC_interaction(
+            η::Real, θ::Real, α::Real, γ_list::Vector, ρ::Real
+        )::DataFrame
+
+    Calculate interaction effect in absolute and relative terms at the country level.
+
+    Use `get_SCC_decomposition` to compute the SCC decomposition at the country level
+    for γ = 0 and γ = 1. Then, calculate the interaction effect as the difference in
+    `present_cost_of_damages_to_E` between the two levels of γ. Finally, calculate the
+    percentage interaction effect relative to the damage when γ = 1.
+
+    Return a DataFrame with the following columns:
+    - `country`: Country code (ISO3).
+    - `inequality_damage_E`: Present cost of damages to environment when γ = 1.
+    - `no_inequality_damage_E`: Present cost of damages to environment when γ = 0.
+    - `interaction`: Absolute interaction effect (difference between the two columns).
+    - `interaction_pct`: Percentage interaction effect relative to `inequality_damage_E`.
+    - `country_id`: Numeric country code for mapping purposes.
+"""
+function get_SCC_interaction(η::Real, θ::Real, α::Real, γ_list::Vector, ρ::Real)::DataFrame
+    SCC_decomposition_df = get_SCC_decomposition(η, θ, α, γ_list, ρ;
+                                                 analysis_level="country")
+
+    no_inequality_df = @chain SCC_decomposition_df begin
+        @filter(γ == 0)
+        @select(country, no_inequality_damage_E = present_cost_of_damages_to_E)
+    end
+
+    inequality_df = @chain SCC_decomposition_df begin
+        @filter(γ == 1)
+        @select(country, inequality_damage_E = present_cost_of_damages_to_E)
+    end
+
+    countries_df = @chain begin
+        DataFrame(all_countries())
+        @select(country = alpha3, country_id = numeric)
+    end
+
+    interaction_df = @chain inequality_df begin
+        @inner_join(no_inequality_df)
+        @mutate(interaction = inequality_damage_E - no_inequality_damage_E)
+        @mutate(interaction_pct = 100 * interaction / inequality_damage_E)
+        @mutate(interaction_pct = ifelse.(interaction .<0,
+                                          interaction_pct .* -1,
+                                          interaction_pct))
+        @mutate(country = string.(country))
+        @left_join(countries_df)
+    end
+
+    return interaction_df
+end
+
+function map_SCC_decomposition_level(interaction_df::DataFrame)
+
+    world110m = dataset("world-110m")
+
+    interaction_map = @vlplot(
+        width = 640,
+        height = 360,
+        title = "",
+        projection = {type = :equirectangular}
+    ) +
+    @vlplot(
+        data = {
+            values = world110m,
+            format = {
+                type = :topojson,
+                feature = :countries
+            }
+        },
+        transform = [{
+            lookup = "id",
+            from = {
+                data = interaction_df,
+                key = :country_id,
+                fields = ["interaction"]
+            }
+        }],
+        mark = :geoshape,
+        encoding = {
+            color = {
+                field = "interaction",
+                type = "quantitative",
+                title = "Interaction cost",
+                scale = {
+                    scheme = "blueorange",
+                    domainMid = 0,
+                    domain = [-0.1, 0.1]
+                }
+            }
+        }
+    )
+
+    return interaction_map
+end
+
+function map_SCC_decomposition_pct(interaction_df::DataFrame)
+
+    world110m = dataset("world-110m")
+
+    percentage_interaction_map = @vlplot(
+        width = 640,
+        height = 360,
+        title = "",
+        projection = {type = :equirectangular}
+    ) +
+    @vlplot(
+        data = {
+            values = world110m,
+            format = {
+                type = :topojson,
+                feature = :countries
+            }
+        },
+        transform = [{
+            lookup = "id",
+            from = {
+                data = interaction_df,
+                key = :country_id,
+                fields = ["interaction_pct"]
+            }
+        }],
+        mark = :geoshape,
+        encoding = {
+            color = {
+                field = "interaction_pct",
+                type = "quantitative",
+                title = "Interaction (%)",
+                scale = {
+                    scheme = "purpleorange",
+                    domainMid = 0
+                }
+            }
+        }
+    )
+
+    return percentage_interaction_map
 end

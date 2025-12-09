d48636a1bf019a8dac0ec6055aaf3601333baefb
rturquier <57546969+rturquier@users.noreply.github.com>
2025-12-09 16:39:52 +0100

    ✨  Structure replication file

--- analysis/replication.jl
+++ analysis/replication.jl
@@ -1,24 +1,25 @@
+# %% Activate environment, install packages and precompile project
 using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
-#Pkg.resolve() # To resolve inconsistencies between Manifest.toml and Project.toml
 Pkg.instantiate()
 
-include("../src/GreenNICE.jl")
+# %% Include
 include("scc.jl")
 
+# %% Set default parameters
 η = 1.5
 θ = 0.5
 α = 0.1
 γ = 1.0
 ρ = 0.01
 γ_list = [0.0, 1.0]
-pulse_year = 2025
-pulse_size = 1.0 # ton CO2
 
-
-## Make figures showing interaction effect at country and region levels
-
+# ==== Map interaction effect at country and region levels ====
+# %% Get data
 country_interaction_df = get_SCC_interaction(η, θ, α, γ_list, ρ)
 
+# %% Absolute interaction map
 absolute_interaction_map = map_SCC_decomposition_level(country_interaction_df)
+
+# %% Relative interaction map
 relative_interaction_map = map_SCC_decomposition_pct(country_interaction_df)

2c72ef6bfd36d9baf594c3e982351573b6535609
iroliva <iroliva@ucdavis.edu>
2025-12-10 11:47:12 -0800

    Save gini vs E_stock0 plot Edit `replication.jl`

--- analysis/replication.jl
+++ analysis/replication.jl
@@ -11,11 +11,13 @@ df_raw = get_descriptives_df()
 
 Initial_E_stock = map_E_percapita_country(df_raw)
 
-E_stock_correlation = plot_E_stock_0_hconcat(df_raw)
+Gini_E_stock0 = plot_gini_E_stock0(df_raw)
 
 Descriptive_plot = plot_descriptive_coefficients(df_raw)
 
+
+
 ## Save figures
 VegaLite.save("outputs/maps/initial_E_stock_percapita.svg", Initial_E_stock)
-VegaLite.save("outputs/figures/E_stock0_hconcat.svg", E_stock_correlation)
+VegaLite.save("outputs/figures/gini_E_stock0.svg", Gini_E_stock0)
 VegaLite.save("outputs/figures/descriptive_coefficients.svg", Descriptive_plot)

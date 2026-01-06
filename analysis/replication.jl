252df436a2a8fa82b934858b9cce564cf54b962c
Rémi Turquier <57546969+rturquier@users.noreply.github.com>
2026-01-06 21:24:30 +0100

    🌳  Check sensitivity to E (#84)
    * 💹  Adjust for inflation using the US BLS API
    
    * 🌲  Get forest value from Costanza et al. (2014)
    
    * 🏃  Plot global flow of E in default run
    
    * 🦧  Get total forest value
    
    * 🧮  Check sensitivity to E
    
    * 📈  Plot sensitivity to E
    
    * 💾  Save sensitivity_to_E.svg
    
    * 💧  Get value of water + food + recreation
    
    ...instead of food + raw materials + generic and medical resources.
    This more or less matches the services included in the CWON data.
    
    * 🤏  Minor clarification
    
    * 🔵  Add additional points to the plot
    
    * ⏪  Revert "🔵  Add additional points to the plot"
    
    This reverts commit f515d1e78.
    
    * ⛲  Add water regulation
    
    * 👾  Convert xls to xslx and upgrade code
    
    * 🪙  Adjust total Costanza estimate for inflation
    
    * 💬  Add a comment

--- analysis/replication.jl
+++ analysis/replication.jl
@@ -46,17 +46,25 @@ save("outputs/maps/map_interaction_effect_pct.svg", absolute_interaction_map)
 relative_interaction_map = map_SCC_decomposition_pct(country_interaction_df)
 save("outputs/maps/map_interaction_effect_pct.svg", relative_interaction_map)
 
+# ==== Descriptive information on the default run ====
+# %% Default run
+m = GreenNICE.create()
+run(m)
+
+# %% Plot temperature trajectory in a default run
+warming_df = getdataframe(m, :damages => :temp_anomaly)
+warming_df |> @vlplot(:line, :time, :temp_anomaly)
+
+# %% Plot global flow of ecosystem services in default run
+# Unit is million of dollars per year
+E_flow_df = getdataframe(m, :environment => :E_flow_global)
+E_flow_df |> @vlplot(:line, :time, :E_flow_global)
+
 # ==== Facet plot ====
 # %% Set default η × θ grid
 η_list = [0.1, 1.05, 2.]
 θ_list = [-1.5, -0.5, 0.5]
 γ_list = [0., 0.5, 1.]
-
-# %% Plot temperature trajectory in a default run
-m = GreenNICE.create()
-run(m)
-warming_df = getdataframe(m, :damages => :temp_anomaly)
-warming_df |> @vlplot(:line, :time, :temp_anomaly)
 
 # %% Run model on parameter grid (this can take a long time) and save results
 facet_df = get_SCC_decomposition(η_list, θ_list, α, γ_list, ρ)
@@ -68,3 +76,22 @@ facet_df = read_csv("outputs/facet_df.csv")
 # %% Facet plot
 facet_plot = facet_SCC(facet_df; cost_to="E")
 facet_plot |> save("outputs/figures/facetted_SCC_decomposition.svg")
+
+# ====  Sensitivity to E
+# %% Get the annual flow of material forest ecosystem services from Costanza et al. (2014)
+costanza_forest_values = get_costanza_forest_values()
+
+# %% Convert total value of ecosystem services to 2017 USD
+total_costanza_estimate = 124.8 * 10^12
+adjust_for_inflation(total_costanza_estimate, 2007, 2017)
+
+# %% Run model with different E multipliers to check sensitivity, and save results
+E_multiplier_list = [0.5, 1, 2, 3, 4, 5]
+
+sensitivity_to_E_df = check_sensitivity_to_E(E_multiplier_list, η, θ, α, ρ)
+write_csv(sensitivity_to_E_df, "outputs/sensitivity_to_E.csv")
+
+# %% Read and plot
+sensitivity_to_E_df = read_csv("outputs/sensitivity_to_E.csv")
+sensitivity_to_E_plot = plot_sensitivity_to_E(sensitivity_to_E_df)
+sensitivity_to_E_plot |> save("outputs/figures/sensitivity_to_E.svg")

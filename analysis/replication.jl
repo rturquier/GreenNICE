9a3988011335ff138f2a2f68897a910832696f92
iroliva <iroliva@ucdavis.edu>
2026-01-19 16:52:30 -0300

    Improve comments

--- analysis/replication.jl
+++ analysis/replication.jl
@@ -14,13 +14,10 @@ initial_E_stock_map = map_E_percapita_country(descriptives_df)
 ξ_map = map_damage_coefficient_country(descriptives_df)
 Gini_E_stock0_scatterplot = plot_gini_E_stock0(descriptives_df)
 
-df = descriptives_df
-
 # %% Save figures
 save("outputs/maps/initial_E_stock_percapita.svg", initial_E_stock_map)
 save("outputs/figures/gini_E_stock0.svg", Gini_E_stock0_scatterplot)
 save("outputs/figures/initial_damage_coefficient_map.svg", ξ_map)
-
 
 # %% Set default parameters
 η = 1.5

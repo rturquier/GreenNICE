16c06f4dc4f0ac2176623798bbc34ffb81715ac2
rturquier <57546969+rturquier@users.noreply.github.com>
2026-03-12 15:54:38 +0100

    ✂️  Split and join df to make it tidy

--- analysis/replication.jl
+++ analysis/replication.jl
@@ -122,7 +122,40 @@ heatmap_df = get_SCC_decomposition(η_list, θ_list, α, [0, 1], ρ)
 write_csv(heatmap_df, "outputs/heatmap_df.csv")
 
 # %% Read
-heatmap_df = read_csv("outputs/heatmap_df.csv")
+heatmap_simulations_df = read_csv("outputs/heatmap_df.csv")
+
+# %% Prepare
+heatmap_df_γ0 = @chain heatmap_simulations_df begin
+    @filter(γ == 0)
+    @select(
+        η,
+        θ,
+        SCC_E_0 = present_cost_of_damages_to_E,
+        SCC_c_0 = present_cost_of_damages_to_c
+    )
+end
+
+heatmap_df_γ1 = @chain heatmap_simulations_df begin
+    @filter(γ == 1)
+    @select(
+        η,
+        θ,
+        SCC_E_1 = present_cost_of_damages_to_E,
+        SCC_c_1 = present_cost_of_damages_to_c
+    )
+end
+
+heatmap_df = @chain begin
+    @left_join(heatmap_df_γ0, heatmap_df_γ1)
+    @mutate(
+        Δ_SCC_E = SCC_E_1 - SCC_E_0,
+        Δ_SCC_c = SCC_c_1 - SCC_c_0,
+    )
+    @mutate(
+        Δ_SCC_E_over_SCC_E = Δ_SCC_E / SCC_E_1,
+        Δ_SCC_E_over_SCC = Δ_SCC_E / (SCC_E_1 + SCC_c_1),
+    )
+end
 
 # ====  Sensitivity to E ====
 # %% Get the annual flow of material forest ecosystem services from Costanza et al. (2014)
